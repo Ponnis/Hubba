bad05671a62eb3e328267ed627f767e3ecc16c83
Jian <jiaanshin@gmail.com>
2018-09-21 14:01:22 +0200

    started on habit class and frequency

--- app/src/main/java/com/example/nils_martin/hubba/Habit.java
+++ app/src/main/java/com/example/nils_martin/hubba/Habit.java
@@ -1,8 +1,18 @@
 package com.example.nils_martin.hubba;
 
+import android.widget.ImageView;
+
 public class Habit {
 
     private String title;
+    private String timestamp;
+    private int streak;
+    private int goalDays;
+    private Frequency frequency;
+    private boolean isDone;
+    private boolean isActive;
+    private boolean enableNofitications;
+    private ImageView image;
 
 
     public Habit(String title){
