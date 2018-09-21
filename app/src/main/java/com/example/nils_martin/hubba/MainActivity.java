bdf0e85e57a0735ca46c140cf94e44494ce541ec
Camilla Söderlund <camsoder@student.chalmers.se>
2018-09-21 15:24:06 +0200

    Kommer till en ny sida när du trycker på knappen

--- app/src/main/java/com/example/nils_martin/hubba/MainActivity.java
+++ app/src/main/java/com/example/nils_martin/hubba/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.nils_martin.hubba;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        init();
     }
+
+
+
+
+    public FloatingActionButton addBtn;
+    public void init() {
+        addBtn = findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddHabit.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
 }
