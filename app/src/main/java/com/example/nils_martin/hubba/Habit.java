c93431e860d69daea547189d8135c4dc5ae98648
Jian <jiaanshin@gmail.com>
2018-09-21 15:51:16 +0200

    some basics added to habit

--- app/src/main/java/com/example/nils_martin/hubba/Habit.java
+++ app/src/main/java/com/example/nils_martin/hubba/Habit.java
@@ -14,8 +14,30 @@ public class Habit {
     private boolean enableNofitications;
     private ImageView image;
 
-
     public Habit(String title){
         this.title = title;
+    }
+
+    public int getGoalDays(Habit habit){
+        return habit.goalDays;
+    }
+
+    public void setGoalDays(Habit habit, int days){
+        habit.goalDays = days;
+    }
+
+    public void setDone(Habit habit){
+        habit.isDone = !habit.isDone;
+        if(habit.isDone){
+            habit.streak++;
+        }
+    }
+
+    public void setActive(Habit habit){
+        habit.isActive = !habit.isActive;
+    }
+
+    private void setNotifications(Habit habit){
+        habit.enableNofitications = !habit.enableNofitications;
     }
 }
