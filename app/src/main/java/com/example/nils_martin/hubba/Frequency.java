9738ae5c9e575cf71214a38b719dbe78b99b5aee
Li Ronning <li.ronning@gmail.com>
2018-09-24 08:57:24 +0200

    added frequencies

--- app/src/main/java/com/example/nils_martin/hubba/Frequency.java
+++ app/src/main/java/com/example/nils_martin/hubba/Frequency.java
@@ -1,5 +1,14 @@
 package com.example.nils_martin.hubba;
 
 public enum Frequency {
-
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY,
+    DAILY,
+    WEEKLY,
+    MONTHLY
 }
