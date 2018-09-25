c23919ad56bf244cb16862cc5a41790432e4c8ea
Li Ronning <li.ronning@gmail.com>
2018-09-25 18:35:03 +0200

    Hardcoded values can now be seen in morningListView

--- app/src/main/java/com/example/nils_martin/hubba/MainActivityController.java
+++ app/src/main/java/com/example/nils_martin/hubba/MainActivityController.java
@@ -5,16 +5,40 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivityController extends AppCompatActivity {
 
-
+    private ListView morningListView ;
+    private ArrayAdapter<String> listAdapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         init();
+
+        // Find the ListView resource.
+        morningListView = (ListView) findViewById( R.id.morningListView );
+
+        // Create and populate a List of planet names.
+        String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
+                "Jupiter", "Saturn", "Uranus", "Neptune"};
+        ArrayList<String> planetList = new ArrayList<String>();
+        planetList.addAll( Arrays.asList(planets) );
+
+        // Create ArrayAdapter using the planet list.
+        listAdapter = new ArrayAdapter<String>(this, R.layout.habit_layout, planetList);
+
+        // Set the ArrayAdapter as the ListView's adapter.
+        morningListView.setAdapter( listAdapter );
+
+
     }
 
 
