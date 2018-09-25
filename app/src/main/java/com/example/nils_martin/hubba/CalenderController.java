c53ab54981b50d28f3d0f6463ffc4a2029085028
Camilla Söderlund <camsoder@student.chalmers.se>
2018-09-25 09:27:07 +0200

    New activity calender
    Skapat en ny view och kompleterande klass

---
+++ app/src/main/java/com/example/nils_martin/hubba/CalenderController.java
@@ -0,0 +1,14 @@
+package com.example.nils_martin.hubba;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class CalenderController extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calender);
+    }
+
+}
