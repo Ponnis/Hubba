65830e4108b4d46a989b8cacd82e6ceccc3e4045
Camilla Söderlund <camsoder@student.chalmers.se>
2018-09-25 10:47:20 +0200

    set Monday to the first day of the week

--- app/src/main/java/com/example/nils_martin/hubba/CalenderController.java
+++ app/src/main/java/com/example/nils_martin/hubba/CalenderController.java
@@ -1,7 +1,9 @@
 package com.example.nils_martin.hubba;
 
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.CalendarView;
 
 public class CalenderController extends Activity {
 
@@ -9,6 +11,13 @@ public class CalenderController extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender);
+        cV = findViewById(R.id.calendarView);
+        cV.setFirstDayOfWeek(2);
+
     }
+
+    CalendarView cV;
+
+
 
 }
