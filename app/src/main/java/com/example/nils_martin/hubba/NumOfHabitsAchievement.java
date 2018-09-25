d8f35ffe55254d09ecedbe373249423f8c1b9e84
Johannes <Jogust95@gmail.com>
2018-09-25 11:39:39 +0200

    HotFix Achievements
    Added String pathToImage to Achievement subclasses

--- app/src/main/java/com/example/nils_martin/hubba/NumOfHabitsAchievement.java
+++ app/src/main/java/com/example/nils_martin/hubba/NumOfHabitsAchievement.java
@@ -4,8 +4,8 @@ import android.widget.ImageView;
 
 public class NumOfHabitsAchievement extends Achievement{
 
-    NumOfHabitsAchievement(String title){
-        super(title);
+    NumOfHabitsAchievement(String title, String pathToImage){
+        super(title, pathToImage);
     }
 
     @Override
--- app/src/main/java/com/example/nils_martin/hubba/StreakAchievement.java
+++ app/src/main/java/com/example/nils_martin/hubba/StreakAchievement.java
@@ -4,8 +4,8 @@ import android.widget.ImageView;
 
 public class StreakAchievement extends Achievement {
 
-    StreakAchievement(String title){
-        super(title);
+    StreakAchievement(String title, String pathToImage){
+        super(title, pathToImage);
     }
 
     @Override
