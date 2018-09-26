25f025dc15f318429af562fb0f83cdfe140cf242
Jian <jiaanshin@gmail.com>
2018-09-26 15:00:11 +0200

    calendar in progress (OBS not working commit!)

---
+++ app/src/main/java/com/example/nils_martin/hubba/CalendarView.java
@@ -0,0 +1,12 @@
+package com.example.nils_martin.hubba;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class CalendarView extends AppCompatActivity{
+
+    CalendarModel calendarModel;
+
+
+}
--- app/src/main/java/com/example/nils_martin/hubba/CalenderController.java
+++ app/src/main/java/com/example/nils_martin/hubba/CalenderController.java
@@ -1,48 +1,18 @@
 package com.example.nils_martin.hubba;
 
-import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.app.Activity;
-import android.widget.CalendarView;
-import android.widget.EditText;
-
-import java.util.Map;
 
 
 public class CalenderController extends Activity {
 
-    CalendarView cV;
-    EditText hej;
-    Calendar calendar;
-
-
+    CalendarModel calendarModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender);
-        init();
+        calendarModel.init();
     }
-
-
-    void init() {
-        cV = findViewById(R.id.calendarView);
-        cV.setFirstDayOfWeek(2);
-        hej = findViewById(R.id.dateText);
-        dateChange();
-        //cV.setMaxDate(cV.getDate()+604800000); //Only one week is clickable
-        //cV.setMinDate(cV.getDate());
-    }
-
-    void dateChange() {
-        cV.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
-                hej.setText("Date: " + i2 + " / " + (i1+1) + " / " + i); //Write date as dd-mm-yy
-            }
-        });
-    }
-
-
 
 }
