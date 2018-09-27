f63bc9c87387252600ae53c0ac1b5a9a0aea97bf
Camilla Söderlund <camsoder@student.chalmers.se>
2018-09-27 16:12:24 +0200

    Tid på event tillagd
    Utvecklat fakehabitklassen(används medan habit är under produktion) med att lägga till en tid när man ska utföra vanan samt lagt till det framför.

--- app/src/main/java/com/example/nils_martin/hubba/CalenderController.java
+++ app/src/main/java/com/example/nils_martin/hubba/CalenderController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class CalenderController extends Activity {
 
-    TextView dateText, aktivitetTxtV;
+    TextView dateText, activityTxtV;
     CalendarView calendarView;
     List<FakeHabit> fake = new ArrayList<>();
 
@@ -23,7 +23,7 @@ public class CalenderController extends Activity {
         setContentView(R.layout.activity_calender);
         calendarView = findViewById(R.id.calendarView);
         dateText = findViewById(R.id.dateText);
-        aktivitetTxtV = findViewById(R.id.aktivitetTxtV);
+        activityTxtV = findViewById(R.id.activityTxtV);
 
         init();
     }
@@ -37,28 +37,28 @@ public class CalenderController extends Activity {
             public void onSelectedDayChange(CalendarView calendarView, int year, int month, int dayOfMonth) {
                 dateText.setText("Date: " + dayOfMonth + " / " + (month+1) + " / " + year);
                 stringBuilder.setLength(0);
-                stringBuilder.append("Aktiviteter:");
+                stringBuilder.append("Habits:");
                 for(int i = 0; i<fake.size(); i++) {
                     if(fake.get(i).date == dayOfMonth) {
                         if (stringBuilder.length() != 0) {          //If you have more than one activity on the same day,
-                            stringBuilder.append("\n" + fake.get(i).name);
+                            stringBuilder.append("\n" + fake.get(i).time + "\b" + fake.get(i).name);
                         }
                         else {
                             stringBuilder.append(fake.get(i).name);
                         }
                     }
                 }
-                aktivitetTxtV.setText(stringBuilder.toString());    //If you have a activity at the day, this will show as a text under the calendar
+                activityTxtV.setText(stringBuilder.toString());    //If you have a activity at the day, this will show as a text under the calendar
             }
         });
     }
 
 
     void makeAList () {
-        fake.add(new FakeHabit("Träning", 7));
-        fake.add(new FakeHabit("Lunch", 4));
-        fake.add(new FakeHabit("Bio", 21));
-        fake.add(new FakeHabit("Hundvakt", 21));
+        fake.add(new FakeHabit("Träning", 7, "12:00"));
+        fake.add(new FakeHabit("Lunch", 4, "14:25"));
+        fake.add(new FakeHabit("Bio", 21, "16:30"));
+        fake.add(new FakeHabit("Hundvakt", 21, "09:05"));
     }
 }
 
@@ -67,9 +67,11 @@ public class CalenderController extends Activity {
 class FakeHabit {
     String name;
     int date;
+    String time;
 
-    FakeHabit (String name, int date) {
+    FakeHabit (String name, int date, String time) {
         this.name = name;
         this.date = date;
+        this.time = time;
     }
 }
