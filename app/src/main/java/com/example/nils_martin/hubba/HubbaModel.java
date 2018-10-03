545a7bc0b81f8d88204810f28d9b91ed7c6a3c99
TheShakiraOfCode <nilsmartin.robeling@gmail.com>
2018-10-03 15:37:23 +0200

    changed small things to adapt hubbaModel to singleton

--- app/src/main/java/com/example/nils_martin/hubba/HubbaModel.java
+++ app/src/main/java/com/example/nils_martin/hubba/HubbaModel.java
@@ -3,7 +3,7 @@ package com.example.nils_martin.hubba;
 import android.widget.EditText;
 
 public class HubbaModel {
-    HubbaServer hubbaServer = new HubbaServer();
+    HubbaServer hubbaServer = HubbaServer.getInstance();
     LoginView loginView = new LoginView();
     EditText currentUserName = loginView.getUsername();
     User currentUser = hubbaServer.getUser(currentUserName.toString());
--- app/src/main/java/com/example/nils_martin/hubba/HubbaServer.java
+++ app/src/main/java/com/example/nils_martin/hubba/HubbaServer.java
@@ -3,7 +3,7 @@ package com.example.nils_martin.hubba;
 import java.util.ArrayList;
 
 public class HubbaServer {
-    
+
     //A server to serve as a mock up for a remote server to hold all the users and control them
     private ArrayList<User> users = new ArrayList<>();
 
@@ -23,10 +23,14 @@ public class HubbaServer {
     public ArrayList<User> getUsers(){
         return users;
     }
-    public HubbaServer getInstance(){
+    /* get the singleton instance*/
+    public static HubbaServer getInstance(){
         if (hubbaServer==null){
             hubbaServer = new HubbaServer();
         }
         return hubbaServer;
+    }
+    public void setUsers(ArrayList<User>users){
+        this.users = users;
     }
 }
