d1c857ebe8c2e5df4481b148ddf0e6a782e2479b
TheShakiraOfCode <nilsmartin.robeling@gmail.com>
2018-10-08 09:28:20 +0200

    Fixed Hubbamodel

--- app/src/main/java/com/example/nils_martin/hubba/HubbaModel.java
+++ app/src/main/java/com/example/nils_martin/hubba/HubbaModel.java
@@ -2,10 +2,32 @@ package com.example.nils_martin.hubba;
 
 import android.widget.EditText;
 
+import java.util.ArrayList;
+
 public class HubbaModel {
-    HubbaServer hubbaServer = HubbaServer.getInstance();
+    private static ArrayList<User> users = new ArrayList<>();
     LoginView loginView = new LoginView();
     EditText currentUserName = loginView.getUsername();
-    User currentUser = hubbaServer.getUser(currentUserName.toString());
+    User currentUser = getUser(currentUserName.toString());
 
+    public User getUser(String userName){
+        int index = 0;
+        for (User user :users){
+            if(user.getName().equals(userName)){
+                index = users.indexOf(user);
+            }
+        }
+        return users.get(index);
+    }
+    public ArrayList<User> getUsers(){
+        return users;
+    }
+
+
+    public void setUsers(ArrayList<User>users){
+        this.users = users;
+    }
+    public void addUser(User user){
+        users.add(user);
+    }
 }
