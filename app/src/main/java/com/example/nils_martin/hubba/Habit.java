d576ddc88f3e275c514416523b3f7f6c7e85e1b2
Camilla Söderlund <camsoder@student.chalmers.se>
2018-10-09 22:19:30 +0200

    The calendar shows the event on the rigth day (only weekly) (part 2)

--- app/src/main/java/com/example/nils_martin/hubba/Habit.java
+++ app/src/main/java/com/example/nils_martin/hubba/Habit.java
@@ -2,6 +2,8 @@ package com.example.nils_martin.hubba;
 
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
 
 public class Habit extends Observable {
@@ -16,6 +18,7 @@ public class Habit extends Observable {
     private boolean enableNofitications;
     private ImageView image;
     private State STATE;
+    private List<Integer> dayToDo = new ArrayList<>();
 
     public Habit(String title){
         this.title = title;
@@ -23,6 +26,15 @@ public class Habit extends Observable {
         this.isDone = false;
         this.isActive = true;
         this.enableNofitications = false;
+    }
+
+    public Habit(String title, List<Integer> days) {
+        this.title = title;
+        this.streak = 0;
+        this.isDone = false;
+        this.isActive = true;
+        this.enableNofitications = false;
+        this.dayToDo = days;
     }
 
     public enum State{
@@ -73,4 +85,12 @@ public class Habit extends Observable {
     }
 
     public State getSTATE (){return STATE;}
+
+    public void setDayToDo (List<Integer> dayToDo) {
+        this.dayToDo = dayToDo;
+    }
+
+    public List<Integer> getDayToDo () {
+        return dayToDo;
+    }
 }
