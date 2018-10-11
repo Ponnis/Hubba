2bec9d8a954290bc2f584249fb0b46872b349d3d
TheShakiraOfCode <nilsmartin.robeling@gmail.com>
2018-10-11 13:53:36 +0200

    Usertest

--- app/src/test/java/com/example/nils_martin/hubba/SavingUserTest.java
+++ app/src/test/java/com/example/nils_martin/hubba/SavingUserTest.java
@@ -1,4 +1,6 @@
 package com.example.nils_martin.hubba;
+import android.os.Bundle;
+
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,10 +11,11 @@ import static org.junit.Assert.assertNotNull;
 public class SavingUserTest {
     @Test
     public void testSaveUser(){
-        LoginView loginView= new LoginView();
         HubbaModel.getInstance().setUsers(new ArrayList<>());
         HubbaModel.addUser(new User("123","123","123"));
-
+        MainActivityController mainActivityController = new MainActivityController();
+        mainActivityController.onPause();
+        assertNotNull(HubbaModel.getUsers());
     }
 
 }
