28c110e8ea380d29fd46a6405f6c84830f9736af
Li Ronning <li.ronning@gmail.com>
2018-10-12 11:28:47 +0200

    Update app/src/main/java/com/example/nils_martin/hubba/MainActivityController.java

--- app/src/main/java/com/example/nils_martin/hubba/MainActivityController.java
+++ app/src/main/java/com/example/nils_martin/hubba/MainActivityController.java
@@ -1,6 +1,7 @@
 package com.example.nils_martin.hubba;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -11,6 +12,10 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ImageButton;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
