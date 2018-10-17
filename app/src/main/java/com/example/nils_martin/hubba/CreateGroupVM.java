b64c2e1a96dd4c6e83a1416565b54a42798193d7
TheShakiraOfCode <nilsmartin.robeling@gmail.com>
2018-10-17 10:43:20 +0200

    Created createdGroupVM

---
+++ app/src/main/java/com/example/nils_martin/hubba/CreateGroupVM.java
@@ -0,0 +1,17 @@
+package com.example.nils_martin.hubba;
+
+import android.widget.EditText;
+
+import com.example.nils_martin.hubba.Model.HubbaModel;
+
+public class CreateGroupVM {
+
+    HubbaModel hubbaModel = HubbaModel.getInstance();
+
+    private EditText  friendUserName;
+
+
+
+    //TODO create method to add checked friends to group list
+
+}
