718883320e048efb805a7240b8eb33826749958f
Johannes <Jogust95@gmail.com>
2018-10-19 17:20:15 +0200

    Bugg fix, you can now go back from myachivements

--- app/src/main/java/com/example/nils_martin/hubba/ViewModel/MenuVM.java
+++ app/src/main/java/com/example/nils_martin/hubba/ViewModel/MenuVM.java
@@ -154,11 +154,7 @@ public class MenuVM extends AppCompatActivity implements ThemableObserver {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MenuVM.this, MyAchievementsVM.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
-                        Intent.FLAG_ACTIVITY_CLEAR_TASK |
-                        Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
-                finish();
             }
         });
     }
