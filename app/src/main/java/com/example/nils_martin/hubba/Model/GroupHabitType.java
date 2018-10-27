70680c7556426f45133ff28c6562d01d3683dda9
TheShakiraOfCode <nilsmartin.robeling@gmail.com>
2018-10-27 16:16:18 +0200

    fix

--- app/src/main/java/com/example/nils_martin/hubba/Model/GroupHabitType.java
+++ app/src/main/java/com/example/nils_martin/hubba/Model/GroupHabitType.java
@@ -1,12 +1,7 @@
 package com.example.nils_martin.hubba.Model;
 
 public class GroupHabitType implements IHabitTypeState {
-    @Override
-    public void updateHabit(Habit habit) {
-       /* if(.isChecked){
-          habit.upGroupMembersDoneCount();
-        }*/
-    }
+
     @Override
     public String toString(){
         return "GroupHabit";
--- app/src/main/java/com/example/nils_martin/hubba/Model/SingleHabitType.java
+++ app/src/main/java/com/example/nils_martin/hubba/Model/SingleHabitType.java
@@ -1,13 +1,7 @@
 package com.example.nils_martin.hubba.Model;
 
 public class SingleHabitType implements IHabitTypeState {
-    @Override
-    public void updateHabit(Habit habit) {
-        //TODO make checkSquare for habit to set done
-        /*if(.isChecked){
-          habit.setDone(habit);
-        }*/
-    }
+
     @Override
     public String toString(){
         return "SingleHabit";
