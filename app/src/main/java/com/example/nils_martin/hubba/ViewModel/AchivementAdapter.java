e3e961a5abc6be8a154a65e7b5bf383382756d02
Johannes <Jogust95@gmail.com>
2018-10-27 12:54:52 +0200

    Pictures for Achievements was set wrong
    Switched places for the pictures so right picture is shown in MyAchievements

--- app/src/main/java/com/example/nils_martin/hubba/ViewModel/AchivementAdapter.java
+++ app/src/main/java/com/example/nils_martin/hubba/ViewModel/AchivementAdapter.java
@@ -64,10 +64,10 @@ public class AchivementAdapter extends RecyclerView.Adapter<AchivementAdapter.Vi
     public int getImage(Achievement achievement) {
        AchievementType achievementType =  achievement.getAchievementType();
        if (achievementType == AchievementType.NumOHabitsAchievement){
-           return R.drawable.streak;
+           return R.drawable.num_of_habits;
        }
        else if (achievementType == AchievementType.StreakAchievement){
-            return R.drawable.num_of_habits;
+            return R.drawable.streak;
        }
        else return R.drawable.profilepic;
     }
