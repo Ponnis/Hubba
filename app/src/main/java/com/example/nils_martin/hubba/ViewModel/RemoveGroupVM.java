fc81d569a2aa60f61b9848c42da07e6cc3646627
Li Ronning <li.ronning@gmail.com>
2018-10-27 16:36:06 +0200

    Added RemoveGroupVM.java

---
+++ app/src/main/java/com/example/nils_martin/hubba/ViewModel/RemoveGroupVM.java
@@ -0,0 +1,66 @@
+package com.example.nils_martin.hubba.ViewModel;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.nils_martin.hubba.Model.HubbaModel;
+import com.example.nils_martin.hubba.Model.ThemableObserver;
+import com.example.nils_martin.hubba.R;
+
+/**
+ * @author Li Rönning
+ */
+public class RemoveGroupVM extends AppCompatActivity implements ThemableObserver {
+
+    private HubbaModel model = HubbaModel.getInstance();
+    private Themehandler themehandler = new Themehandler();
+
+    private Button yesButton;
+    private Button noButton;
+    private TextView groupTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(themehandler.getTheme());
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.remove_group);
+        themehandler.addThemeListener(this);
+        init();
+    }
+
+    private void init() {
+        initFindByView();
+        initOnClickListener();
+    }
+
+    private void initFindByView() {
+        yesButton = findViewById(R.id.yesGroupBtn);
+        noButton = findViewById(R.id.noGroupBtn);
+        groupTextView = findViewById(R.id.groupTextView);
+    }
+
+    private void initOnClickListener() {
+        yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO remove group when save works
+            }
+        });
+
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+
+    @Override
+    public void recreateActivity() {
+        recreate();
+    }
+}
