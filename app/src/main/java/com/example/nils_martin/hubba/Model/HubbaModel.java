801cb5a5a2febf3e402ed3559884ab745ede6111
Li Ronning <li.ronning@gmail.com>
2018-10-27 16:24:39 +0200

    added comments

--- app/src/main/java/com/example/nils_martin/hubba/Model/HubbaModel.java
+++ app/src/main/java/com/example/nils_martin/hubba/Model/HubbaModel.java
@@ -2,7 +2,7 @@ package com.example.nils_martin.hubba.Model;
 
 import java.util.ArrayList;
 /**
- * @author Nils-Martin Robeling
+ * @author Nils-Martin Robeling, Li Rönning
  * */
 public class HubbaModel implements IHubbaModel {
     private static HubbaModel instance = null;
